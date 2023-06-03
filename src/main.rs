15176964173311bbbb243721f10870a3180494d8
Michel Romero <studentenherz@gmail.com>
2023-06-03 02:10:29 -0300

    Add database handler get_list

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
+mod database;
 mod handle_inline;
 mod handle_message;
 
 use dotenvy::dotenv;
+use teloxide::prelude::*;
+
+use database::DatabaseHandler;
 use handle_inline::handle_inline;
 use handle_message::handle_message;
-use teloxide::prelude::*;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+
+    let db_handler = DatabaseHandler::from_env().await;
+    print!("{:?}", db_handler.get_list("ho").await);
 
     pretty_env_logger::init();
     log::info!("Starting command bot...");
